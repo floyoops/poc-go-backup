02b30ecab61d09fa8dc895dd48322968c5afe20b
Florian Chaudret <florian.chaudret@gmail.com>
2019-11-26 00:08:35 +0100

    Parse yml by args

--- src/backup/backup.go
+++ src/backup/backup.go
@@ -19,30 +19,56 @@ type ParametersDb struct {
 	}
 }
 
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	// Open
-	pathYmlFile := "data/parameters.yml"
-	file, err := os.Open(pathYmlFile)
+	// get args.
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Please enter a yml file path")
+		os.Exit(1)
+	}
+	pathFile := args[1]
+
+	// Check if file exist.
+	if Exists(pathFile) == false {
+		fmt.Printf("the file '%v' does not exist\n", pathFile)
+		os.Exit(1)
+	}
+	fmt.Printf("Path file yml detected: %v\n", pathFile)
+
+	// Open.
+	file, err := os.Open(pathFile)
 	if err != nil {
 		log.Fatalf("error on open file: %v\n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("open %v success\n", pathYmlFile)
+	fmt.Printf("open %v success\n", pathFile)
 
 	// Read
 	defer file.Close()
 	byteValue, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Fatalf("error on read %s\n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("read %v success\n", pathYmlFile)
+	fmt.Printf("read %v success\n", pathFile)
 
 	// Unmarshal
 	p := ParametersDb{}
 	err = yaml.Unmarshal([]byte(byteValue), &p)
 	if err != nil {
 		log.Fatalf("error: %v", err)
+		os.Exit(1)
 	}
-	fmt.Printf("unmarshall %v success\n", pathYmlFile)
+	fmt.Printf("unmarshall %v success\n", pathFile)
 
 	fmt.Printf("dbs %v", p.Parameters)
 }
