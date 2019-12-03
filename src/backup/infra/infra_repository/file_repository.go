81f3ad7ef0bd58b3caf9111436f44569732ebfea
Florian Chaudret <florian.chaudret@gmail.com>
2019-12-04 00:47:10 +0100

    Add teet file repository

--- src/backup/infra/infra_repository/file_repository.go
+++ src/backup/infra/infra_repository/file_repository.go
@@ -21,15 +21,14 @@ func (r FileRepository) exists(name string) bool {
 func (r FileRepository) GetContent(pathFile string) ([]byte, error) {
 	// Check if file exist.
 	if r.exists(pathFile) == false {
-		fmt.Errorf("the file '%v' does not exist\n", pathFile)
+		return make([]byte, 0, 0), fmt.Errorf("the file '%v' does not exist\n", pathFile)
 	}
 	fmt.Printf("Arg path file yml detected: %v\n", pathFile)
 
 	// Open.
 	file, err := os.Open(pathFile)
 	if err != nil {
-		fmt.Errorf("error on open file: %v\n", err)
-		return make([]byte, 0, 0), err
+		return make([]byte, 0, 0), fmt.Errorf("error on open file: %v\n", err)
 	}
 	fmt.Printf("open %v success\n", pathFile)
 
@@ -37,8 +36,7 @@ func (r FileRepository) GetContent(pathFile string) ([]byte, error) {
 	defer file.Close()
 	byteValue, err := ioutil.ReadAll(file)
 	if err != nil {
-		fmt.Errorf("error on read %s\n", err)
-		return make([]byte, 0, 0), err
+		return make([]byte, 0, 0), fmt.Errorf("error on read %s\n", err)
 	}
 	fmt.Printf("Read success")
 
---
+++ src/backup/infra/infra_repository/file_repository_test.go
@@ -0,0 +1,19 @@
+package infra_repository
+
+import "testing"
+
+func TestGetContentSuccess(t *testing.T)  {
+	repo := FileRepository{}
+	_, err := repo.GetContent("../../../../data/parameters.yml")
+	if err != nil {
+		t.Errorf("no %v", err)
+	}
+}
+
+func TestFileNotExist(t *testing.T)  {
+	repo := FileRepository{}
+	_, err := repo.GetContent("fake")
+	if err.Error() != "the file 'fake' does not exist\n" {
+		t.Errorf("no")
+	}
+}
