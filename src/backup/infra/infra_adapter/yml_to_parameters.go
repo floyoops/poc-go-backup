15e15c2688075a5179bed33a522c14e576cde6eb
Florian Chaudret <fchaudret@leadersleague.com>
2019-12-03 18:50:08 +0100

    Yml dynamic

--- src/backup/infra/infra_adapter/yml_to_parameters.go
+++ src/backup/infra/infra_adapter/yml_to_parameters.go
@@ -17,5 +17,17 @@ func (c YmlToParameters) Convert(byteValue []byte) (model.ParametersDb, error)  
 		fmt.Errorf("error: %v", err)
 	}
 
+	var result map[string]map[string]string
+	err2 := yaml.Unmarshal([]byte(byteValue), &result)
+	if err2 != nil {
+		fmt.Errorf("error %v", err2)
+	}
+	p.Parameters.DbDriver = result["parameters"]["database_driver"]
+	p.Parameters.DbHost = result["parameters"]["database_host"]
+	p.Parameters.DbPort = result["parameters"]["database_port"]
+	p.Parameters.DbName = result["parameters"]["database_name"]
+	p.Parameters.DbUser = result["parameters"]["database_user"]
+	p.Parameters.DbPassword = result["parameters"]["database_password"]
+
 	return p, err
 }
