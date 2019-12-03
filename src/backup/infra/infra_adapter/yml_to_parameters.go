ef7e7046f30b4ee7e10536f0bed4848658c60cc9
Florian Chaudret <fchaudret@leadersleague.com>
2019-12-03 18:56:15 +0100

    return if error yml

--- src/backup/infra/infra_adapter/yml_to_parameters.go
+++ src/backup/infra/infra_adapter/yml_to_parameters.go
@@ -15,12 +15,14 @@ func (c YmlToParameters) Convert(byteValue []byte) (model.ParametersDb, error)  
 	err := yaml.Unmarshal([]byte(byteValue), &p)
 	if err != nil {
 		fmt.Errorf("error: %v", err)
+		return p, err
 	}
 
 	var result map[string]map[string]string
 	err2 := yaml.Unmarshal([]byte(byteValue), &result)
 	if err2 != nil {
 		fmt.Errorf("error %v", err2)
+		return p, err2
 	}
 	p.DbHost = result["parameters"]["database_host"]
 	p.DbPort = result["parameters"]["database_port"]
