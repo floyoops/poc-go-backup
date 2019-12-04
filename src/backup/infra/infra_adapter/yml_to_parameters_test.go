316e82e2827c14b580d66bb9105af3a00b7fe7ee
Florian Chaudret <florian.chaudret@gmail.com>
2019-12-04 02:25:48 +0100

    Add test yml converter with different types of yml file

--- src/backup/infra/infra_adapter/yml_to_parameters_test.go
+++ src/backup/infra/infra_adapter/yml_to_parameters_test.go
@@ -1,32 +1,70 @@
 package infra_adapter
 
 import (
+	"fmt"
 	"github.com/floyoops/poc-go-backup/src/backup/domain/model"
 	"github.com/floyoops/poc-go-backup/src/backup/infra/infra_repository"
 	"testing"
 )
 
-func TestSuccess(t *testing.T)  {
+var Expected  = model.ParametersDb{
+	DbHost: "db",
+	DbPassword: "root_password",
+	DbName: "the_db_name",
+	DbPort: "",
+	DbUser: "root",
+}
+
+func FileYmlToModelExpect(filePath string, expected model.ParametersDb) (bool, error)  {
 	repo := infra_repository.FileRepository{}
-	filePath := "../../../../data/parameters.yml"
 	bytesValue, errorRepo := repo.GetContent(filePath)
 	if errorRepo != nil {
-		t.Errorf("error get content file '%v'", filePath)
+		return false, fmt.Errorf("error get content file '%v'", filePath)
 	}
 	converter := YmlToParameters{}
 	result, errorConvert := converter.Convert(bytesValue)
 	if errorConvert != nil {
-		t.Errorf("Error on convert the content of '%v'", filePath)
-	}
-	expected := model.ParametersDb{
-		DbHost: "db",
-		DbPassword: "root_password",
-		DbName: "the_db_name",
-		DbPort: "",
-		DbUser: "root",
+		return false, fmt.Errorf("error on convert the content of '%v'", filePath)
 	}
 	if result != expected {
-		t.Errorf("the result %v different of expected %v", result, expected)
+		return false, fmt.Errorf("the result %v different of expected %v", result, Expected)
+	}
+	return true, nil
+}
+
+func TestSuccess(t *testing.T)  {
+	_, err := FileYmlToModelExpect("../../../../data/parameters.yml", Expected)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestYmlDisorderSuccess(t *testing.T)  {
+	_, err := FileYmlToModelExpect("../../../../data/parameters_disorder.yml", Expected)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestYulMinimalSuccess(t *testing.T)  {
+	_, err := FileYmlToModelExpect("../../../../data/parameters_minimal.yml", Expected)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestYmlEmptyFail(t *testing.T)  {
+	_, err := FileYmlToModelExpect("../../../../data/parameters_empty.yml", Expected)
+	if err == nil {
+		t.Error("expected error not found")
+	}
+}
+
+func TestYmlIncompleteFail(t *testing.T)  {
+	_, err := FileYmlToModelExpect("../../../../data/parameters_incomplete.yml", Expected)
+	errorExpected := "the result {db    } different of expected {db  the_db_name root root_password}"
+	if err != nil && err.Error() != errorExpected {
+		t.Errorf("error message '%v' is different of expected message: '%v'", err.Error(), errorExpected)
 	}
 }
 
